ba55c7293245c5d5994b4d3d849c7560832617ea
alanSechi <alan.sechi@gmail.com>
2020-12-05 22:41:00 +0100

    [ADD] Layout coplete. There is still to do the slider with javascript.

--- script.js
+++ script.js
@@ -16,19 +16,19 @@ accordionItemHeaders.forEach(accordionItemHeader => {
 
 
 function accordionMenu(question) {
-    // let keyframes = [
-    //     { opacity: "0" },
-    //     { height: "auto" },
-    //     { height: "auto" },
-    //     { opacity: "1" },
-    // ];
+    let keyframes = [
+        { opacity: "0" },
+        { height: "auto" },
+        { height: "auto" },
+        { opacity: "1" },
+    ];
     let answer = question.currentTarget.nextElementSibling;
-    question.currentTarget.classList.toggle("active");
-    if (answer.classList.contains("active") && !answer.style.maxHeight) {
-        answer.classList.remove("active");
+    question.currentTarget.classList.toggle("expanded");
+    if (answer.classList.contains("expanded") && !answer.style.maxHeight) {
+        answer.classList.remove("expanded");
         answer.style.maxHeight = answer.scrollHeight + "px";
     } else {
-        answer.classList.add("active");
+        answer.classList.add("expanded");
         answer.style.maxHeight = null;
     }
 }
@@ -69,4 +69,63 @@ function loadMore() {
         newQuest.addEventListener("click", accordionMenu);
     }
     document.getElementById("loadMoreButton").style.display = "none";
+}
+
+
+var myInput = document.getElementById("psw");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+    document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+    document.getElementById("message").style.display = "none";
+}
+
+// When the user starts to type something inside the password field
+myInput.onkeyup = function() {
+    // Validate lowercase letters
+    var lowerCaseLetters = /[a-z]/g;
+    if (myInput.value.match(lowerCaseLetters)) {
+        letter.classList.remove("invalid");
+        letter.classList.add("valid");
+    } else {
+        letter.classList.remove("valid");
+        letter.classList.add("invalid");
+    }
+
+    // Validate capital letters
+    var upperCaseLetters = /[A-Z]/g;
+    if (myInput.value.match(upperCaseLetters)) {
+        capital.classList.remove("invalid");
+        capital.classList.add("valid");
+    } else {
+        capital.classList.remove("valid");
+        capital.classList.add("invalid");
+    }
+
+    // Validate numbers
+    var numbers = /[0-9]/g;
+    if (myInput.value.match(numbers)) {
+        number.classList.remove("invalid");
+        number.classList.add("valid");
+    } else {
+        number.classList.remove("valid");
+        number.classList.add("invalid");
+    }
+
+    // Validate length
+    if (myInput.value.length >= 8) {
+        length.classList.remove("invalid");
+        length.classList.add("valid");
+    } else {
+        length.classList.remove("valid");
+        length.classList.add("invalid");
+    }
 }
